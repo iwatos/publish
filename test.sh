eb6e8d011858a2df7898ba42fa100a5a1f553c9f
iwatos <nonbiriactive@gmail.com>
2021-12-31 12:27:28 +0900

    update

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 find ./docs/publish -name '*.md' | while read file
 do
     name=`basename "${file}" .md`
-    sed -i "" "1s|^|#\ ${name}\n|" "${file}"
+    sed -i "1s|^|#\ ${name}\n|" "${file}"
 done
