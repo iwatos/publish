c228f8ea59eb8b40f341b8eae06403d91da2feb0
iwatos <nonbiriactive@gmail.com>
2021-12-31 13:39:41 +0900

    update

--- test.sh
+++
@@ -1,4 +0,0 @@
-find ./docs/publish -name '*.md' | while read file
-do
-    gsed -ie "s|\([ \n\r\f\t]\+\)\(#[^ \n\r\f\t]\+\)|\1\`\2\`|g" "${file}"
-done
---
+++ update.sh
@@ -0,0 +1,7 @@
+rm -rf ~/repos/publish/docs/ &&
+mkdir ~/repos/publish/docs/ &&
+cp -R ~/obsidian/private/publish/** ~/repos/publish/docs/ &&
+cp ~/repos/publish/README.md ~/repos/publish/docs/index.md &&
+git add -A &&
+git commit -m "update" &&
+git push
