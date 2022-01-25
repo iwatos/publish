288b3c4ce26a376cb38352c0021b5c9c0447d025
iwatos <nonbiriactive@gmail.com>
2022-01-26 00:22:06 +0900

    update

--- src/update.sh
+++ src/update.sh
@@ -23,7 +23,7 @@ cp "$ROOT/README.md" "$DOCS/index.md"
 cp -R "$ROOT/assets" "$DOCS/assets"
 
 mkdir "$DOCS/attachments"
-grep -rE "!\[\[.*.(png|jpg|jpeg|pdf)\]\]" ./docs/ | grep -oE "!\[\[.*.(png|jpg|jpeg|pdf)\]\]" | sed -e 's/!\[\[//g' -e  's/\]\]//g' | while read line
+grep -rE "!\[\[.*.(png|jpg|jpeg|pdf)\]\]" "$DOCS/" | grep -oE "!\[\[.*.(png|jpg|jpeg|pdf)\]\]" | sed -e 's/!\[\[//g' -e  's/\]\]//g' | while read line
 do
   cp "/Users/yt/obsidian/private/attachments/$line" "$DOCS/attachments/$line"
 done
